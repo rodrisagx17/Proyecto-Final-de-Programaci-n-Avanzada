aff91aeb7f2ebe516a4a1f4b82ecf825a86d8687
Alexander Sotelo <98243876+Alexander-Sot23@users.noreply.github.com>
2025-11-10 14:46:51 -0600

    Refactor Cliente class to use Persona and new attributes

--- CODIGO/MODELS/cliente.py
+++ CODIGO/MODELS/cliente.py
@@ -1,34 +1,15 @@
 from datetime import datetime
 from typing import Optional
+from MODELS.persona import Persona
 from MODELS.sede import Sede
 
 class Cliente:
-    def __init__(self, id: Optional[int] = None, identificacion: str = "", 
-                 nombre: str = "", apellido_paterno: str = "", apellido_materno: str = "",
-                 telefono: str = "", genero: str = "", fecha_nacimiento: datetime = None,
-                 fecha_inscripcion: datetime = None, sede_inscrito: Optional[Sede] = None,
-                 activo: bool = True):
-        self.id = id
-        self.identificacion = identificacion
-        self.nombre = nombre
-        self.apellido_paterno = apellido_paterno
-        self.apellido_materno = apellido_materno
-        self.telefono = telefono
-        self.genero = genero
-        self.fecha_nacimiento = fecha_nacimiento
-        self.fecha_inscripcion = fecha_inscripcion
+    def __init__(self, cod_cli: Optional[int] = None, persona: Optional[Persona] = None, 
+                 sede_inscrito: Optional[Sede] = None, genero: str = "", 
+                 fecha_nac: datetime = None, fecha_inscri: datetime = None):
+        self.cod_cli = cod_cli
+        self.persona = persona or Persona()
         self.sede_inscrito = sede_inscrito
-        self.activo = activo
-
-    @property
-    def nombre_completo(self) -> str:
-        return f"{self.nombre} {self.apellido_paterno} {self.apellido_materno}"
-
-    @property
-    def edad(self) -> int:
-        if self.fecha_nacimiento is None:
-            return 0
-        hoy = datetime.now()
-        return hoy.year - self.fecha_nacimiento.year - (
-            (hoy.month, hoy.day) < (self.fecha_nacimiento.month, self.fecha_nacimiento.day)
-        )
+        self.genero = genero
+        self.fecha_nac = fecha_nac
+        self.fecha_inscri = fecha_inscri
