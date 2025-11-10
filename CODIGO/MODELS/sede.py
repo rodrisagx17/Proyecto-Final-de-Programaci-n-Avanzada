54a8c87f93a04f5ad03b311807f52f84e14a45b8
Alexander Sotelo <98243876+Alexander-Sot23@users.noreply.github.com>
2025-11-10 14:46:35 -0600

    Refactor Sede class to use Direccion object

--- CODIGO/MODELS/sede.py
+++ CODIGO/MODELS/sede.py
@@ -1,18 +1,16 @@
 from typing import Optional
+from MODELS.direccion import Direccion
 
 class Sede:
     def __init__(self, id: Optional[int] = None, nombre: str = "", hora_abre: str = "", 
-                 hora_cierra: str = "", direccion: str = "", telefono: str = "", 
-                 ciudad: str = "", estado: str = "", cp: int = 0):
+                 hora_cierra: str = "", direccion: Optional[Direccion] = None, 
+                 telefono: str = ""):
         self.id = id
         self.nombre = nombre
         self.hora_abre = hora_abre
         self.hora_cierra = hora_cierra
-        self.direccion = direccion
+        self.direccion = direccion or Direccion()
         self.telefono = telefono
-        self.ciudad = ciudad
-        self.estado = estado
-        self.cp = cp
 
     def __str__(self):
-        return f"{self.nombre} - {self.direccion}, {self.ciudad}"
+        return f"{self.nombre} - {self.direccion}"
